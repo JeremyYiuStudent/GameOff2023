35d6a16bd75d635fe6943c105e1e3211c3f26cc0
Jeremy Yiu <jeremyyiu.student@gmail.com>
2023-11-20 11:45:26 -0800

    Initial Commit
    This branch is for developing the feature of the color mixing puzzle component.

---
+++ Assets/Scripts/ColorMixer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ColorMixer : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private bool mixerUp = false;
+    
+    private int holder;
+
+    private string[] colorTable = {"Red", "Yellow", "Purple", "Green", "", "Cyan", "", "", "Blue"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J)){
+            mixerUp = !mixerUp;
+        }
+    }
+    void OnGUI(){
+        if(mixerUp){
+            if(GUI.Button(new Rect(100,0,100,30), "Red")){
+                if(holder == 0){holder = 1;}
+                else{
+                    Debug.Log(text);
+                    text.text = colorTable[holder*1-1];
+                    holder = 0; mixerUp = false;
+                }
+            }
+            if(GUI.Button(new Rect(200,0,100,30), "Green")){
+                if(holder == 0){holder = 2;}
+                else{
+                    Debug.Log(text);
+                    text.text = colorTable[holder*2-1];
+                    holder = 0; mixerUp = false;
+                }
+            }
+            if(GUI.Button(new Rect(300,0,100,30), "Blue")){
+                if(holder == 0){holder = 3;}
+                else{
+                    text.text = colorTable[holder*3-1];
+                    Debug.Log(text);
+                    holder = 0; mixerUp = false;
+                }
+            }
+        }
+    }
+}
