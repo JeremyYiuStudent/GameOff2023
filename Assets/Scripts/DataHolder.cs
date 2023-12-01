f8f33164580ab5d5e00926f29340020a4008abeb
Jeremy Yiu <jeremyyiu.student@gmail.com>
2023-12-01 11:42:59 -0800

    More Dump And More

--- Assets/Scripts/DataHolder.cs
+++ Assets/Scripts/DataHolder.cs
@@ -8,5 +8,5 @@ public class DataHolder
     //{PaintShop, Streets, Office, Hotel, Vault, Subway, Skydive}
     public static bool[] quickTravel = {true, true, false, false, false, false, false};
 
-    public static int maxJump = 0;
+    public static int maxJump = 1;
 }
